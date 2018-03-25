b759c4b532b6eece35217790bfdd0395ce59862f
Patter.L <atpboy444@hotmail.com>
2018-03-25 09:22:18 +0800

    step 2 prepare to add reg infomation to DeviceWindow: 1 added reg filed name to table 2 add idx bar for page row 3 add title for page 4 prepare use PageElement into MessageView unit 5 test phy deivces, and prepare re-structure each row elem 7 prepare adjust recycleview 8 before change title content from recyclelayout to boxlayout 9 fixed issue of view in recyclelayoutbox, prepare dele some rubbish ==================================================================== 10 add rsv seq mark before desc created 11 fixed issue in splitting compound id table (like is is 2-5 in tab) 12 RecycleView has some bug for fresh height = self.minimum_height, adjust it may manually ==================================================================== 13 add TextInput for reg value input 14 temp commit before add LayerBehavior 15 action could report to WidgetPageElement 16 page write is working ===================================================================== 17 add read/write button control, but a window issue is found in main runing 18 the more window refrered to DeviceWindow.kv(research later) 19 fixed bug for write command no respond when data is same ===================================================================== 20 temp commit for re-write PageElement compatible with reporter 21 preliminary add msg repo view, but T100 muti id not combined 22 try to test idx heigh == data heigt, but has some issue 23 fixed default select issue ===================================================================== 24 fixed issue for default tab selection 25 fixed issue in unfocus input writing 26 fixed issue for recycle layout fresh ===================================================================== 27 prepare to add message bar 28 added message bar and changed shutcut, prepare delete LayoutBehavior Length and item ===================================================================== 29 added message content, has some bugs still need to be fixed 30 before add hook in message view 31 added paint view =================================================================== 32 temporarily commmit, for inspect software architecture 33 change phy event to handle message directly; fixed some bugs in reading with zero feedback; andd timeout in device windown command; add IRQ PHY command =================================================================== log_parser update prepare invovle 3D mode ===================================================================

--- tools/log_parser.py
+++ tools/log_parser.py
@@ -31,20 +31,49 @@ class reference(object):
         row_name = ["X{}".format(i) for i in range(mx)]
         col_name = ["Y{}".format(i) for i in range(my)]
         self.data = pd.DataFrame(np_data, index=row_name, columns=col_name)
-        print(self.data)
+        #print(self.data)
+        print("Get page", n)
 
-class mxt_app_log(loader):
-    PARSING_WORD = {'MXTAPP': {'sep':',', 'title':r'[xX](\d+)[yY](\d+)', 'row_name':"%H:%M:%S.%f", 'st': 2, 'end': None},
-                    'HAWKEYE': {'sep': ',', 'title': r'[xX](\d+)[yY](\d+)', 'row_name': "%H:%M:%S %f", 'st': 1, 'end': None}}
+    def max(self, size=None):
+        if isinstance(size, (tuple, list)):
+            xsize, ysize = size
+            d = self.data.iloc[:xsize, :ysize]
+        else:
+            d = self.data
+        return d.max().max()
 
-    def __init__(self, type, filename=None):
+    def min(self, size=None):
+        if isinstance(size, (tuple, list)):
+            xsize, ysize = size
+            d = self.data.iloc[:xsize, :ysize]
+        else:
+            d = self.data
+
+        return d.min().min()
+
+class DebugViewLog(loader):
+    FILE_FORMAT = {'mxtapp': {'sep':',', 'title':r'[xX](\d+)[yY](\d+)', 'row_name':"%H:%M:%S.%f", 'st': 2, 'end': None},
+                    'hawkeye': {'sep': ',', 'title': r'[xX](\d+)[yY](\d+)', 'row_name': "%H:%M:%S %f", 'st': 1, 'end': None},
+                    'studio': {'sep': ',', 'title': r'[xX](\d+)[yY](\d+)', 'row_name': "%H:%M:%S %f", 'st': 1, 'end': None}}
+
+    def __init__(self, type, size, filename=None):
         super().__init__()
 
         self.title = None
-        self.parsing_word = self.PARSING_WORD[type]
-        self.matrix_xy = None
+        self.parsing_word = self.FILE_FORMAT[type]
+        self.channel_size = size
+        self.matrix_size = None
         self.load_file(filename)
         self.frames = []
+
+    @classmethod
+    def supported_format(cls, format):
+        return format in cls.FILE_FORMAT.keys()
+
+    def set_maxtrix_size(self, size):
+        self.matrix_size = size
+        if not self.channel_size:
+            self.channel_size = size    #default value for channel size
 
     def parse(self, filename=None):
         if filename:
@@ -55,7 +84,7 @@ class mxt_app_log(loader):
 
         del self.frames[:]
 
-        for line in list(self.f):
+        for i, line in enumerate(list(self.f)):
             if line.isspace():
                 continue
 
@@ -64,7 +93,8 @@ class mxt_app_log(loader):
                 content.pop()   #remove null tail
 
             if not self.title:
-                channel_list = content[2:]
+                #channel_list = content[2:]
+                channel_list = content[self.parsing_word['st']:self.parsing_word['end']]
                 pat = re.compile(self.parsing_word['title'])
 
                 mx, my = (0, 0)
@@ -78,13 +108,20 @@ class mxt_app_log(loader):
                         if my < y:
                             my = y
 
-                self.matrix = (mx + 1, my + 1)
+                size = (mx + 1, my + 1)
+                self.set_maxtrix_size(size)
                 self.title = channel_list[0].split('_')[0]
             else:
                 try:
                     dt = datetime.strptime(content[0], self.parsing_word['row_name'])
-                    n = int(content[1])
-                    v = np.array(list(map(int, content[self.parsing_word['st']:self.parsing_word['end']]))).reshape(self.matrix)
+                    st = self.parsing_word['st']
+                    end = self.parsing_word['end']
+                    if st > 1:
+                        n = int(content[1])
+                    else:
+                        n = i
+
+                    v = np.array(list(map(int, content[st: end]))).reshape(self.matrix_size)
                     ref = reference(dt, n ,v)
                     self.frames.append(ref)
                 except:
@@ -92,20 +129,144 @@ class mxt_app_log(loader):
 
         self.close_file()
 
-    def save_to_file(self, output=None):
+    def save_to_file(self, limit, output=None):
         if not output:
             output = self.filename + '.xlsx'
 
         writer = pd.ExcelWriter(output)
 
         for frame in self.frames:
+            if limit:
+                inside, low, hight = limit
+                v_max = frame.max(self.channel_size)
+                v_min = frame.min(self.channel_size)
+                if v_max <= hight and v_min >= low:
+                    v_inside = True
+                else:
+                    v_inside = False
+
+                if v_inside != inside:
+                    continue
+
+            print("Save page", frame.n)
             frame.data.to_excel(writer, sheet_name=str(frame.n))
 
         writer.save()
 
+        print("Save to:", output)
+
 if __name__ == '__main__':
-    log = mxt_app_log('HAWKEYE')
-    #filename = r"D:\trunk\customers2\BYD\12.8_Qin100_1664s\log\ref2.log"
-    filename = r"D:\trunk\tools\maXStudio control files\Hawkeye_20180125_190246.csv"
-    log.parse(filename)
-    log.save_to_file()
+    # log = DebugViewLog('HAWKEYE')
+    # #filename = r"D:\trunk\customers2\BYD\12.8_Qin100_1664s\log\ref2.log"
+    # filename = r"D:\trunk\tools\maXStudio control files\Hawkeye_20180125_190246.csv"
+    # log.parse(filename)
+    # log.save_to_file()
+
+    import os
+    import sys
+    import argparse
+
+    def parse_range(limit_txt):
+        if not limit_txt:
+            return
+
+        limit_txt = limit_txt.strip()
+        pat = re.compile(r'\^?\((-?\d+)[ \t]*,[ \t]*(-?\d+)\)')
+        if limit_txt:
+            result = pat.match(limit_txt)
+            if result:
+                low, high = result.groups()
+                if limit_txt[0] == '^':
+                    inside = False
+                else:
+                    inside = True
+                limit = (inside, int(low), int(high))
+                print("Set limit:", limit)
+                return limit
+
+    def parse_size(size_txt):
+        if not size_txt:
+            return size_txt
+
+        size_txt = size_txt.strip()
+        pat = re.compile(r'\((-?\d+)[ \t]*,[ \t]*(-?\d+)\)')
+        if size_txt:
+            result = pat.match(size_txt)
+            if result:
+                low, high = result.groups()
+                size = (int(low), int(high))
+                print("Set size:", size)
+                return size
+
+    def runstat(args=None):
+        parser = parse_args(args)
+        aargs = args if args is not None else sys.argv[1:]
+        args = parser.parse_args(aargs)
+        print(args)
+
+        if not args.filename and not args.type:
+            parser.print_help()
+            return
+
+        format = args.type
+        if not DebugViewLog.supported_format(format):
+            print("Unsupported type", format)
+            return
+
+        limit = parse_range(args.range)
+        size = parse_size(args.size)
+
+        path = args.filename
+        if path:
+            if os.path.exists(path):
+                log_parser = DebugViewLog(format, size)
+                log_parser.parse(path)
+                log_parser.save_to_file(limit)
+            else:
+                print('Un-exist file name \'{:s}\''.format(path))
+
+    def parse_args(args=None):
+
+        parser = argparse.ArgumentParser(
+            prog='xparse',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            description='Tools for parsing debug log to xlxs file')
+
+        parser.add_argument('--version',
+                            action='version', version='%(prog)s v1.0.0',
+                            help='show version')
+
+        parser.add_argument('-f', '--filename', required=True,
+                            nargs='?',
+                            default='',
+                            metavar='LOG_FILE',
+                            help='where the \'XCFG|TXT\' file will be load')
+
+        parser.add_argument('-t', '--type', required=True,
+                            nargs='?',
+                            default='',
+                            const='.',
+                            metavar='hawkeye|mxtapp|studio',
+                            help='format of of file data content')
+
+        parser.add_argument('-r', '--range', required=False,
+                            nargs='?',
+                            default='',
+                            const='.',
+                            metavar='^(low, hight)',
+                            help='value range to save result, store to (low, high), ^ mean not in range')
+
+        parser.add_argument('-s', '--size', required=False,
+                            nargs='?',
+                            default='',
+                            const='.',
+                            metavar='^(XSIZE, YSIZE)',
+                            help='value to told XSIZE/YSIZE')
+
+        return parser
+
+
+    cmd = None
+    #cmd = r'-t mxtapp -r ^(19000,28000) -s (30,52) -f D:\trunk\customers2\BYD\12.8_Qin100_1664s\log\ref2.log.csv'.split()
+    #cmd = r"-t studio -r ^(-100,100) -f  D:\trunk\customers2\Desay\Desay_DFLZM_SX7_10.1Inch_641T_14804_Goworld\log\Graphical_Debug_Viewer_Log_19_三月_11_22_48.csv".split()
+    runstat(cmd)
